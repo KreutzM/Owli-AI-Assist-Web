72613bd9f949a3af84a682a3882ad87673d0e6b9
Michak <121102425+KreutzM@users.noreply.github.com>
2026-08-02 09:07:17 +0200

    build(web): type staging video export flag

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -8,6 +8,7 @@ interface ImportMetaEnv {
   readonly VITE_OWLI_VERSION_CODE?: string;
   readonly VITE_OWLI_DEFAULT_LOCALE?: string;
   readonly VITE_OWLI_STAGING_PROTOTYPE_MEDIARECORDER?: 'enabled';
+  readonly VITE_OWLI_STAGING_BRANDED_VIDEO_EXPORT?: 'enabled';
   readonly VITE_OWLI_BUILD_TARGET?: string;
   readonly VITE_OWLI_GIT_SHA?: string;
   readonly VITE_OWLI_GIT_DIRTY?: 'true' | 'false';
